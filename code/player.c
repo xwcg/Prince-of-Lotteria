f52259f789076e7f817c10123466555a41488a62
Christian Behrenberg <christian@behrenberg.de>
2013-07-05 21:25:18 +0200

    Jetpack etwas besser zeitkorrigiert + FBX raus
    - das accelerate bei der Gravity hat die Zeitkorrektur auf jedenfall
    negative beeinflusst, weil accelerate selber schon mit time_step^2 die
    Beschleunigung rechnet und da auf keinen Fall eine time_step korrigierte
    Beschleunigung reingehen darf - eine linear zeitkorrigierte gravity
    sieht auf jedenfall fast genauso aus wie zuvor und bei niedriger
    framerate ist die Zeitkorrektur nun besser (!)
    - das heißt aber nicht, dass die issue komplett weg ist. Wenn ich die
    Zeitkorrektur rauslasse und erst kurz vor dem c_move reintue, dann
    bricht mir der playercode... mal sehen, ob wir das noch vernünftig
    gefixedt bekommen
    - ein paar FBX Dateien die keiner braucht habe ich mal gelöscht
    - den PL_DEBUG debug-cheat habe ich mal in die cheat liste in der main
    aufgenommen

--- code/player.c
+++ code/player.c
@@ -75,31 +75,64 @@ void animate_player(var _speed) {
 	}
 }
 
-void move_player() {
+void actPlayer ()
+{
+	move_friction = 0.3;
+	ent_preload(my);
 	
+	player = my;
+	
+	my->PL_JUMP_TIME = 1;
+	my->PL_JUMP_HEIGHT = PL_MAX_JUMP_HEIGHT;
+	my->PL_IS_JUMPING = 0;
+	my->PL_PLAYER_POS_Y = my->y;
+	my->PL_HEALTH = 3;
+	my->PL_A4_COUNT = 0;
+	my->trigger_range = 100;
+	
+	c_setminmax(my);
+	
+	wait(1);
+	
+	vec_set(my->min_x, vector(-40, -40, -55)); 
+	vec_set(my->max_x, vector(40, 40, 60));
+	
+	zorroMeshOptions(me, false, false, false);
+	
+	move_player();
+}
+
+void move_player ()
+{
 	int nTraceDown = 0;
 	int nTraceUp = 0;
 	var vFallSpeed = 0;
 	int nSndRandom = 0;
+	
 	VECTOR vecPlayerMoveSpeed;
-	vec_set(vecPlayerMoveSpeed,nullvector);
+	vec_set(vecPlayerMoveSpeed, nullvector);
 	
 	while (1)
 	{
 		player.PL_JUMP_HEIGHT = PL_MAX_JUMP_HEIGHT;
-		if(key_force.x > 0){my.pan = 0;}
-		if(key_force.x < 0){my.pan = -180;}
 		
-		// Move player with cursor keys
-		vecPlayerMoveSpeed.x = 35 * (key_cur - key_cul) * time_step;
-		vecPlayerMoveSpeed.y = 0;
+		if (key_force.x > 0)
+			my.pan = 0;
+			
+		if (key_force.x < 0)
+			my.pan = -180;
 		
-		// Jump on curor up AND if player doesn't jump atm
-		if ((key_cuu == 1) && (player.PL_IS_JUMPING == 0)) {
+		// walk
+		vecPlayerMoveSpeed.x = g_playerWalkSpeed * key_force.x * time_step;
+		
+		// jump
+		if ((key_force.y > 0) && (player.PL_IS_JUMPING == 0))
+		{
 			player.PL_IS_JUMPING = 1;
 			my.PL_ANIMATION_PERCENTAGE = 0;
-			nSndRandom = integer(random(5));
-			switch(nSndRandom) {
+
+			switch ((int)(random(5)))
+			{
 				case 0: snd_play(sndJump01, 100, 0); break;
 				case 1: snd_play(sndJump02, 100, 0); break;
 				case 2: snd_play(sndJump03, 100, 0); break;
@@ -109,84 +142,88 @@ void move_player() {
 		}
 		
 		// If player is jumping...
-		if (player.PL_IS_JUMPING == 1) {
+		if (player.PL_IS_JUMPING)
+		{
 			#ifdef PL_DEBUG
-				draw_text("Is jumping", 10, 60, COLOR_BLUE);
+				draw_text("player is jumping", 10, 60, COLOR_BLUE);
 			#endif
 			
-			// ... and should keep jumping ...
-			if (player.PL_JUMP_TIME > -1) {
-				
-				// ... then move him up in the air
+			if (player.PL_JUMP_TIME > -1)
+			{
 				player.PL_JUMP_TIME -= 0.2 * time_step;
-				vecPlayerMoveSpeed.z = player.PL_JUMP_HEIGHT * time_step * player.PL_JUMP_TIME;
+				vecPlayerMoveSpeed.z = player.PL_JUMP_HEIGHT * player.PL_JUMP_TIME * time_step;
 			}
 		}
 	
-		// Using jetpack?
-		if (flying_man) {
-			if (key_space) {
-				if (fPlayerJetpack > 0) {
-					
-					// Play jetpack loop	
-					if (!snd_playing(vPlayerSndHandle)) {
+		// jetpack
+		if (flying_man)
+		{
+			if (key_space)
+			{
+				if (fPlayerJetpack > 0)
+				{
+					// play jetpack loop	
+					if (!snd_playing(vPlayerSndHandle))
 						vPlayerSndHandle = ent_playsound(player, sndJetpackLoop, 500);
-					}
 					
-					// Play wohoooo
-					if (vPlayerWoho == 0) {
+					// wohoooo
+					if (vPlayerWoho == 0)
+					{
 						nSndRandom = integer(random(100));
-						if (nSndRandom < 25) {
+						
+						if (nSndRandom < 25)
 							snd_play(sndJetpack01, 1000, 0);
-						} else {
-							if (nSndRandom < 50) {
+						else
+							if (nSndRandom < 50)
 								snd_play(sndJetpack02, 1000, 0);
-							} else {
-								if (nSndRandom < 75) {
+							else
+								if (nSndRandom < 75)
 									snd_play(sndJetpack03, 1000, 0);
-								} else {
-									if (nSndRandom < 98) {
+								else
+									if (nSndRandom < 98)
 										snd_play(sndJetpack04, 1000, 0);
-									} else {
+									else
 										snd_play(sndJetpack05, 1000, 0);
-									}
-								}
-							}
-						}
+										
 						vPlayerWoho = 1;
 					}
+					
 					player.PL_IS_JUMPING = 1;
 					player.PL_JUMP_TIME += 0.25 * time_step;
 					
 					#ifdef PL_DEBUG
-						draw_text("Accel up:", 10, 330, COLOR_RED);
-						draw_text(str_for_num(NULL, player.PL_JUMP_TIME), 200, 330, COLOR_RED);
+						draw_text(str_printf(NULL, "player accel up: %.3f", (double)player->PL_JUMP_TIME), 10, 330, COLOR_RED);
 					#endif
 					
 					player.PL_JUMP_TIME = minv(player.PL_JUMP_TIME, PL_JETPACK_MAX_STRENGTH);
-					fPlayerJetpack -=0.1 * time_step;
+					fPlayerJetpack -= 0.1 * time_step;
 					fPlayerJetpackCooldown = 10;
 					vecPlayerMoveSpeed.z += player.PL_JUMP_TIME * time_step;
 					
-					if (my.pan == 0) {
+					if (my.pan == 0)
 						effect(pJetpack,maxv(1,10*time_frame),vector(player.x-10,player.y,player.z+5),vector(0,0,-3));
-					} else {
+					else
 						effect(pJetpack,maxv(1,40*time_frame),vector(player.x+10,player.y,player.z+5),vector(0,0,-3));
-					}
 				}
-			} else {
+			}
+			else
+			{
 				
-				if (vPlayerSndHandle != 0) snd_stop(vPlayerSndHandle);
-				// Regenerate jetpack
-				if (fPlayerJetpackCooldown > 0) {
-					fPlayerJetpackCooldown -=0.4 * time_step;
-					if (fPlayerJetpackCooldown < 0) {
+				if (vPlayerSndHandle != 0)
+					snd_stop(vPlayerSndHandle);
+					
+				// regenerate jetpack
+				if (fPlayerJetpackCooldown > 0)
+				{
+					fPlayerJetpackCooldown -= 0.4 * time_step;
+					
+					if (fPlayerJetpackCooldown < 0)
 						fPlayerJetpackCooldown = 0;
-					}
-				} else {
-					if (fPlayerJetpack < PL_JETPACK_MAX_FUEL) {
-						fPlayerJetpack +=PL_JETPACK_REFRESH_STEP * time_step;
-					}
+				}
+				else
+				{
+					if (fPlayerJetpack < PL_JETPACK_MAX_FUEL)
+						fPlayerJetpack += PL_JETPACK_REFRESH_STEP * time_step;
 				}
 			}
 		}
@@ -213,7 +250,7 @@ void move_player() {
 				
 				// Let him fall
 				player.PL_JUMP_TIME = -0.1;
-				vecPlayerMoveSpeed.z = player.PL_JUMP_HEIGHT * time_step * player.PL_JUMP_TIME;
+				vecPlayerMoveSpeed.z = player.PL_JUMP_HEIGHT * player.PL_JUMP_TIME * time_step;
 			} else {
 				vecPlayerMoveSpeed.z = -0.1 * time_step; // error 0
 			}
@@ -236,24 +273,28 @@ void move_player() {
 			}
 		}
 		
-		// If there is space under the player let him fall
-		if(nTraceDown > 15 || !HIT_TARGET) {
+		if (nTraceDown > 15 || !HIT_TARGET)
+		{
 			#ifdef PL_DEBUG
 				draw_text("Should be falling", 10, 250, COLOR_GREEN);
 				draw_text(str_for_num(NULL, vecPlayerMoveSpeed.z), 150, 250, COLOR_GREEN);
 			#endif
-			//accelerate(vecPlayerMoveSpeed.z, -PL_GRAVITY * time_step, 0.2);
-			vecPlayerMoveSpeed.z += accelerate(vFallSpeed, -PL_GRAVITY * time_step, 0.2);
-		} else {
-			// If player is on ground ...
-			if((nTraceDown <= 10)) {
-				// Allow him to jump again
-				if (!key_cuu) {
+			
+			vecPlayerMoveSpeed.z += -PL_GRAVITY * time_step;
+		}
+		else
+		{
+			// on ground
+			if (nTraceDown <= 10)
+			{
+				if (!key_cuu)
+				{
 					player.PL_IS_JUMPING = 0;
 					player.PL_JUMP_TIME = PL_MAX_JUMP_TIME;
 					vFallSpeed = 0;
 					
-					if (!key_space) vPlayerWoho = 0;
+					if (!key_space)
+						vPlayerWoho = 0;
 				}
 			}	
 		}
@@ -394,9 +435,11 @@ void move_player() {
 			draw_text(str_for_num(NULL, my.PL_HEALTH), 10, 10, COLOR_RED);
 		#endif
 		
-		// Execute the move
-		move_friction = 0; // Fix by Felix, Verhindert 80% aller Stucks
-		c_move(me,nullvector,vecPlayerMoveSpeed.x,IGNORE_PASSABLE | IGNORE_PASSENTS | GLIDE | ACTIVATE_TRIGGER);
+		move_friction = 0;
+		
+		//vec_scale(&vecPlayerMoveSpeed, time_step);
+		c_move(me, nullvector, &vecPlayerMoveSpeed, IGNORE_PASSABLE | IGNORE_PASSENTS | GLIDE | ACTIVATE_TRIGGER);
+		
 		my.y = my.PL_PLAYER_POS_Y;
 		
 		animate_player(vecPlayerMoveSpeed.x * time_step);
@@ -446,31 +489,8 @@ void move_player() {
 	}
 }
 
-void actPlayer ()
+void activate_jetpack ()
 {
-	if (sky_active == 0)
-		skychange();
-
-	move_friction = 0.3; // Fixed by padmalcom to avoid to get stuck on slopes
-	ent_preload(me);
-	player = me;
-	player.PL_JUMP_TIME = 1;
-	player.PL_JUMP_HEIGHT = PL_MAX_JUMP_HEIGHT;
-	player.PL_IS_JUMPING = 0;
-	player.PL_PLAYER_POS_Y = my.y;
-	player.PL_HEALTH = 3;
-	player.PL_A4_COUNT = 0;
-	player.trigger_range = 100;
-	c_setminmax(player);
-	wait(1);
-	vec_set(player.min_x,vector(-40,-40,-55)); 
-	vec_set(player.max_x,vector(40,40,60));
-	
-	zorroMeshOptions(me, FALSE, FALSE, FALSE);
-	move_player();
-}
-
-void activate_jetpack() {
 	flying_man = 1;
 	zorroMeshOptions(player, TRUE, FALSE, TRUE);
 }
